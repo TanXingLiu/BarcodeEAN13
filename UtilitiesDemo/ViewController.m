d354169316b3418a655c68c1e6ea121b8edec531
XingLiuTan <630242148@qq.com>
2016-07-21 15:42:57 +0800

    viewDidLoad加入NSLog(@"测试");

--- UtilitiesDemo/ViewController.m
+++ UtilitiesDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "BarCodeView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"测试");
 }
 
 - (void)didReceiveMemoryWarning {
